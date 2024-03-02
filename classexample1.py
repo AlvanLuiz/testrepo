9ab1a6a0fc3bb994668b7254a607bbb36610efaf
j.lui <j.lui@digipen.edu>
2024-03-02 10:44:53 +0800

    added class example

---
+++ classexample1.py
@@ -0,0 +1,10 @@
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+car1 = Car("Toyota", "Camry", 2020)
+car2 = Car("Ford", "Mustang", 2022)
+car420 = Car("Tesla", "cybertruck", 2019)
+print(car2.model)
